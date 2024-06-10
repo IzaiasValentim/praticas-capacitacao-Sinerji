f71e7fadec0e8d7ac593d2411e700175d53a56b6
Izaias Valentim <izaias451@gmail.com>
2024-06-10 07:59:50 -0300

    Andamento da seção 6 - Arrays e Collections.

---
+++ arrays-e-collections/src/Desafios/DesafioArray.java
@@ -0,0 +1,23 @@
+package Desafios;
+
+import java.util.Scanner;
+
+public class DesafioArray {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Informe a quanitdade de notas: ");
+        int quantidade = entrada.nextInt();
+
+        double []notas = new double[quantidade];
+        for(int i=0; i<quantidade; i++){
+            System.out.printf("Informe a %d nota: ",i+1);
+            notas[i] = entrada.nextDouble();
+        }
+        double total =0.0;
+        for(double nota : notas){
+            total += nota;
+        }
+        double media = total/quantidade;
+        System.out.println("A media das notas é: "+media);
+    }
+}
