1aef054339d756124f18937aeedda87babb90f04
Izaias Valentim <izaias451@gmail.com>
2024-06-14 12:06:20 -0300

    Adição dos exercícios feitos na seção 14.

---
+++ banco-de-dados-relacional/src/Exercicios/InserirNovosEstadosInvalidos.sql
@@ -0,0 +1,5 @@
+INSERT INTO estados (id, nome, sigla, regiao, populacao)
+VALUES (1000, 'Novo', 'NV', 'SUL', 2.54);
+
+INSERT INTO estados (nome, sigla, regiao, populacao)
+VALUES ('Mais Novo', 'MN', 'NORTE', 2.51);
---
+++ banco-de-dados-relacional/src/Exercicios/criarTabelaPrefeito.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS prefeitos (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    cidade_id int unsigned,
+    PRIMARY KEY (id),
+    UNIQUE KEY (cidade_id),
+    FOREIGN KEY (cidade_id) REFERENCES cidades (id)
+);
+
+-- cidade_id está sem o NOT NULL de propósito. Para fins demonstrativos.
