a3d3a89b79710734e25848bdcdce0aae6506fa48
Izaias Valentim <izaias451@gmail.com>
2024-06-22 11:56:36 -0300

    A  lista de todos e controle de estado foi passada para App.jsx. Isto pois o TodosContext.Provider irá disponibilizar essas dados para a aplicação.

--- 2.ReactJs-Completo-com-Redux-Toolkit-Firebase-Git-Typescript/exercicios/projeto-secao-2-3/src/App.jsx
+++ 2.ReactJs-Completo-com-Redux-Toolkit-Firebase-Git-Typescript/exercicios/projeto-secao-2-3/src/App.jsx
@@ -1,17 +1,29 @@
 import Header from './components/Header.jsx';
 import Home from './views/Home.jsx';
 import './App.scss';
+import { TodosContext } from './TodosContext.js';
+import { useState } from 'react';
+
+// Array com alguns todos base.
+const initialTodos = [
+  { id: 0, title: 'Do Groceries', description: 'Buy apples, rice, juice and toilet paper.', isDone: true },
+  { id: 1, title: 'Study React', description: 'Understand context & reducers.', isDone: false },
+  { id: 2, title: 'Learn Redux', description: 'Learn state management with Redux', isDone: false }
+];
 
 function App() {
+
+  // Definição do state para os todos.
+  const [todos, setTodos] = useState(initialTodos);
 
   return (
     <>
       <main>
-        
-        <Header appName="To-Do List with React JS" />
+        <TodosContext.Provider value={{ todos, setTodos }}>
+          <Header appName="To-Do List with React JS" />
 
-        <Home />
-
+          <Home />
+        </TodosContext.Provider>
       </main>
     </>
   )
--- 2.ReactJs-Completo-com-Redux-Toolkit-Firebase-Git-Typescript/exercicios/projeto-secao-2-3/src/components/TodosList.jsx
+++ 2.ReactJs-Completo-com-Redux-Toolkit-Firebase-Git-Typescript/exercicios/projeto-secao-2-3/src/components/TodosList.jsx
@@ -1,28 +1,22 @@
-import { useState } from 'react';
+import { useContext } from 'react';
+import { TodosContext } from '../TodosContext.js';
 import Todo from './Todo.jsx';
-
-// Array com alguns todos base.
-const initialTodos = [
-  { id: 0, title: 'Do Groceries', description: 'Buy apples, rice, juice and toilet paper.', isDone: true },
-  { id: 1, title: 'Study React', description: 'Understand context & reducers.', isDone: false },
-  { id: 2, title: 'Learn Redux', description: 'Learn state management with Redux', isDone: false }
-];
 
 function TodosList() {
   // Definição do state para os todos.
-  const [todos, setTodos] = useState(initialTodos);
+  const store = useContext(TodosContext);
 
   // Função que deleta um todo pelo id.
   function deleteHandler(id) {
     if (confirm(' Tem certeza que desejas apagar a tareffa?')) {
-      setTodos(todos.filter(todo => todo.id !== id));
+      store.setTodos(store.todos.filter(todo => todo.id !== id));
     }
   }
 
   // função que muda o valor lógico de isDone de um todo.
   function toggleIsDoneHandler(id) {
 
-    setTodos(todos.map(todo => {
+    store.setTodos(store.todos.map(todo => {
       if (todo.id === id) {
         todo.isDone = !todo.isDone;
         return todo;
@@ -40,7 +34,7 @@ function TodosList() {
     <>
       <div className="todos">
 
-        {todos.map(todo =>
+        {store.todos.map(todo =>
 
           <Todo
             todo={todo}
