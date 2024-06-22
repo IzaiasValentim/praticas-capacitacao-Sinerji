f5028d639b1ba728102d3ed24c6b4263686d1b6e
Izaias Valentim <izaias451@gmail.com>
2024-06-22 11:29:00 -0300

    Adicionado os eventon onClick que chamam as funções passadas por props.

--- 2.ReactJs-Completo-com-Redux-Toolkit-Firebase-Git-Typescript/exercicios/projeto-secao-2-3/src/components/Todo.jsx
+++ 2.ReactJs-Completo-com-Redux-Toolkit-Firebase-Git-Typescript/exercicios/projeto-secao-2-3/src/components/Todo.jsx
@@ -1,27 +1,29 @@
 import './Todo.scss';
 
-function Todo({ todo }) {
+function Todo({ todo, deleteTodo, toggleIsDone}) {
 
-  return (
-    <>
-        <div className={`todo ${todo.isDone ? 'done' : ''}`}>
-            <button className="erase">x erase</button>
-            <h3>
-                {todo.title}
-            </h3>
-            <p>
-                {todo.description}
-            </p>
-            <div className="task-check">
-                <input type="checkbox" checked={todo.isDone} />
-                <label>
-                    {!todo.isDone ? 'To-Do' : 'Done'}
-                </label>
+    return (
+        <>
+            <div className={`todo ${todo.isDone ? 'done' : ''}`}>
+                <button onClick={() => deleteTodo(todo.id)} className="erase">
+                    x erase
+                </button>
+                <h3>
+                    {todo.title}
+                </h3>
+                <p>
+                    {todo.description}
+                </p>
+                <div className="task-check">
+                    <input onClick={() => toggleIsDone(todo.id)} type="checkbox" checked={todo.isDone} />
+                    <label>
+                        {!todo.isDone ? 'To-Do' : 'Done'}
+                    </label>
+                </div>
             </div>
-        </div>
 
-    </>
-  )
+        </>
+    )
 }
 
 export default Todo
