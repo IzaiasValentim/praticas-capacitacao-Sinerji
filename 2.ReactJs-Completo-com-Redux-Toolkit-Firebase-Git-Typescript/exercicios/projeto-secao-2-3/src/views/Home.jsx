ea2ebff7dfc514cc591f41894481fa5cf80cf963
Izaias Valentim <izaias451@gmail.com>
2024-06-22 14:07:21 -0300

    Adição dos componentes que irão possibilitar a adição de uma tarefa.

--- 2.ReactJs-Completo-com-Redux-Toolkit-Firebase-Git-Typescript/exercicios/projeto-secao-2-3/src/views/Home.jsx
+++ 2.ReactJs-Completo-com-Redux-Toolkit-Firebase-Git-Typescript/exercicios/projeto-secao-2-3/src/views/Home.jsx
@@ -1,13 +1,19 @@
 import Filter from '../components/Filter.jsx';
 import TodosList from '../components/TodosList.jsx';
+import AddTodoModal from "../components/modals/AddTodoModal.jsx"
+import ModalWindow from "../components/modals/ModalWindow.jsx"
+
 
 function Home() {
 
     return (
     <>
+        <ModalWindow>
+            <AddTodoModal/>
+        </ModalWindow>
         <div className="container">
             <Filter />
-        
+
             <TodosList />
         </div>
 
