eff5c76080b2c40c5d308b11168c68fe604bf2bb
Izaias Valentim <izaias451@gmail.com>
2024-06-22 11:12:55 -0300

    Adição de alguns comentários.

--- 2.ReactJs-Completo-com-Redux-Toolkit-Firebase-Git-Typescript/exercicios/projeto-secao-2-3/src/components/TodosList.jsx
+++ 2.ReactJs-Completo-com-Redux-Toolkit-Firebase-Git-Typescript/exercicios/projeto-secao-2-3/src/components/TodosList.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Todo from './Todo.jsx';
 
+// Array com alguns todos base.
 const initialTodos = [
   { id: 0, title: 'Do Groceries', description: 'Buy apples, rice, juice and toilet paper.', isDone: true },
   { id: 1, title: 'Study React', description: 'Understand context & reducers.', isDone: false },
@@ -8,10 +9,13 @@ const initialTodos = [
 ];
 
 function TodosList() {
-
+  // Definição do state para os todos.
   const [todos, setTodos] = useState(initialTodos);
 
   return (
+    /*
+      Map itera todos os todos da lista e possibilita a construção de todos os componente Todo com as props(todo, key) enviadas.
+    */
     <>
       <div className="todos">
 
